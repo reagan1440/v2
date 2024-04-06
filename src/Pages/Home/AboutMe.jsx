489f00604a04adb30d77ea38dd1ed19ed53e2ca5
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-04-06 14:04:17 -0400

    main

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <img className="educationimg" src="./img/1.png" alt="About Me" />
+        </div>
+      </div>
+      <div className="about--section--img">
+        <a target="_blank" rel="noopener noreferrer">
+          <img src="./img/port2.png" alt="About Me" className='port2' />
+        </a>
+      </div>
+      
+    </section>
+  );
+}
+
+
+  
+  
