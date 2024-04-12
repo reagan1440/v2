43b82a7a8b32de397a0fcd6858863bdd5fc72188
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-04-12 01:15:27 -0400

    routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
